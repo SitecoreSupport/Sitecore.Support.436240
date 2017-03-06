a81922b09274a149197a6ebba62fba4857f22683
ido <ido@WS-IDO1UA1>
2017-03-06 19:36:22 +0200

    Base push

---
+++ src/Sitecore.Support.436240/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.436240")]
+[assembly: AssemblyProduct("Sitecore.Support.436240")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("The chrome parameter may be null on adding a new component in the Experience Editor")]
+
